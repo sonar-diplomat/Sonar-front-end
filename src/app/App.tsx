1df482e37d35a362efd2b325ca96a4661ffdb084
Wirdigo <91600271+Wirdigo@users.noreply.github.com>
2025-10-14 00:43:38 +0300

    Feature: Add reusable Button light style component with styles and examples

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './styles/index.css';
 import {TabBar} from "../widgets";
+import ButtonExamples from "../shared/ui/Button/Button.example.tsx";
 
 function App() {
   return (
     <div className="app">
       <h1>Sonar Application</h1>
+        <ButtonExamples/>
         <TabBar/>
     </div>
   );
