d8f6a06eb826317fb9609dc364ad0a1f70420032
telrond <163304906+TelrondRocklaer@users.noreply.github.com>
2025-12-11 20:00:30 +0200

    feat: Set up capacitor npx cap add android / ios npm run build npx cap sync npx cap run android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ua.pp.sonar_dev',
+  appName: 'Sonar',
+  webDir: 'dist'
+};
+
+export default config;
